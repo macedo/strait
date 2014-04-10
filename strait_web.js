b8073a5ac24e978b5974ea86165799cb5db41404
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-04-10 11:02:40 -0300

    wip

--- strait_web.js
+++ strait_web.js
@@ -1,18 +1,18 @@
-var express = require("express")
-  , http    = require("http")
-  , path    = require("path")
-  , exphbs  = require("express3-handlebars")
-  , config  = require("config.json")("./config.json")
-  , router  = require("./lib/router");
+var express  = require("express")
+  , http     = require("http")
+  , path     = require("path")
+  , exphbs   = require("express3-handlebars")
+  , config   = require("config.json")("./config.json")
+  , util     = require("util")
+  , socketio = require("socket.io")
+  , Strait   = require("./lib/strait")
+  , router   = require("./lib/router");
 
 var App = express();
 
 App.engine("handlebars", exphbs({ defaultLayout: "application" }));
 
 App.set("config", config);
-App.set("pool", Array.apply(null, Array(config.pool.size)).map(function(_, i) {
-  return config.pool.offset + i;
-}));
 App.set("views", __dirname + "/views");
 App.set("view engine", "handlebars");
 
@@ -25,4 +25,14 @@ App.use("/", express.static(path.join(__dirname, "public")));
 
 router.initialize(App);
 
-http.createServer(App).listen(3300);
+server = http.createServer(App);
+server.listen(config.port);
+
+var io = socketio.listen(server);
+io.set("log level", 0);
+io.sockets.on("connection", function(client) {
+  console.log("new connection");
+  new Strait(client, "binary");
+});
+
+util.log("Server is listening on localhost:" + config.port);
