51b4a7442e80d16d8811fdd739a6bd148b3d35ed
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-04-14 15:17:43 -0300

    add mediator js file

---
+++ client/src/mediator.js
@@ -0,0 +1,6 @@
+module.exports = Mediator = {
+  obj: $({}),
+  publish: function(event, data) { return this.obj.trigger(event, data); },
+  subscribe: function(event, fn) { return this.obj.on(event, fn); },
+  unsubscribe: function(event, fn) { return this.obj.off(event, fn); }
+};
